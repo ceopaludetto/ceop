f9551b8abb21350f4331b7bd1f22952e7fa88f68
Carlos Eduardo <ceo.paludetto@gmail.com>
2021-11-03 13:59:50 -0300

    fix(plugin-typescript): ts-loader dependency

--- packages/plugin-typescript/src/index.ts
+++ packages/plugin-typescript/src/index.ts
@@ -4,7 +4,10 @@ const plugin: Plugin = (configuration) => {
 	removeBabelPluginsOrPresets(configuration, "presets", "@babel/preset-typescript");
 	findRuleByRegex(configuration, "tsx?", (rule) => {
 		if (Array.isArray(rule.use)) {
-			rule.use.push({ loader: "ts-loader", options: { transpileOnly: true, configFile: normalize("tsconfig.json") } });
+			rule.use.push({
+				loader: require.resolve("ts-loader"),
+				options: { transpileOnly: true, configFile: normalize("tsconfig.json") },
+			});
 		}
 
 		return rule;
