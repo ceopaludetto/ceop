364beb72ca2b8776e9feba6b6143e6fb9dc6ae78
Carlos Eduardo <ceo.paludetto@gmail.com>
2021-12-14 12:59:01 -0300

    feat: remove lerna, add turborepo, add two new packages, eslint and typescript

--- packages/utils/.eslintrc.js
+++ packages/utils/.eslintrc.js
@@ -1,8 +1,1 @@
-const path = require("path")
-
-module.exports = {
-  extends: ["../../.eslintrc.js"],
-  parserOptions: {
-		project: path.resolve("tsconfig.json"),
-	},
-}
+module.exports = require("@ceop/eslint")
